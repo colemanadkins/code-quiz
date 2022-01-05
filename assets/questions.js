4afe63a8e9800b14c4f720f40ab52280c683f072
Coleman Adkins <ladiesluvcoleman@yahoo.com>
2022-01-04 19:22:55 -0500

    Add questions to array

--- assets/questions.js
+++ assets/questions.js
@@ -1,2 +1,13 @@
 // Create an array of questions to use for the quiz
-var questions = []
+var questions = [
+    {
+        title: "Who plays Billy Ray Valentine in the movie 'Trading Places?'", 
+        choice: ["Denzel Washington", "Eddie Murphy", "Al Pacino", "Will Smith"],
+        answer: "Eddie Murphy"
+    },
+    {
+        title: "What is the name of Bruce Willis' character in the movie 'The Fifth Element'",
+        choice: ["Korben Dallas", "Michael Corben", "Ruby Rhod", "Zorg"],
+        answer: "Korben Dallas"
+    },
+]
