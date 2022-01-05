2a3bc942533950c12d44d697dfd2186497be8a1c
Coleman Adkins <ladiesluvcoleman@yahoo.com>
2022-01-04 20:08:58 -0500

    Adding questions to array

--- assets/questions.js
+++ assets/questions.js
@@ -1,7 +1,7 @@
 // Create an array of questions to use for the quiz
 var questions = [
     {
-        title: "Who plays Billy Ray Valentine in the movie 'Trading Places?'", 
+        title: "Who plays Billy Ray Valentine in the movie 'Trading Places'?", 
         choice: ["Denzel Washington", "Eddie Murphy", "Al Pacino", "Will Smith"],
         answer: "Eddie Murphy"
     },
@@ -10,6 +10,22 @@ var questions = [
         choice: ["Korben Dallas", "Michael Corben", "Ruby Rhod", "Zorg"],
         answer: "Korben Dallas"
     },
+    {
+        title: "In the movie 'Tombstone', what is the name of the gang terrorizing the town?",
+        choice: ["The Outlaws", "The Snakes", "Dirty Dogs", "The Cowboys"],
+        answer: "The Cowboys"
+    },
+    {
+        title: "What popular actress played Kate aka Acid Burn in the movie 'Hackers'?", 
+        choice: ["Kate Hudson", "Cate Blanchett", "Angelina Jolie", "Reese Witherspoon"],
+        answer: "Angelina Jolie"
+    },
+    {
+        title: "What is the name of the movie starring Robert De Niro playing a deranged Vietnam War veteran name Travis Bickle?",
+        choice: ["Taxi Driver", "Raging Bull", "Meet The Parents", "Space Jam"],
+        answer: "Taxi Driver"
+    },
+    
 ];
 // Testing input in console log
 console.log(questions[1].choice[0]);
--- assets/style.js
+++ assets/style.js
@@ -8,3 +8,36 @@ var startBtn = document.querySelector("#start");
 var questionsElement = document.querySelector("#questions");
 var timerElement = document.querySelector("#time");
 var questionChoices = document.querySelector("#choices");
+
+// Create function to start the quiz
+function startQuiz() {
+    var startScreen = document.querySelector("#start-screen");
+    startScreen.setAttribute("class", "hide");
+    // Unhide the questions
+    questionsElement.removeAttribute("class");
+
+    getCurrentQuestion();
+}
+
+// Get current question and choices
+function getCurrentQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleElement = document.querySelector("#question-title");
+    titleElement.textContent = currentQuestion.title;
+
+    questionChoices.textContent = "";
+
+    //*** Alternate using "for loop" instead of "forEach" */
+for (var i=0; i < currentQuestion.choice.length; i++) {
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", currentQuestion.choice[i]);
+
+    choiceNode.textContent = i + 1 + ". " + currentQuestion.choice[i];
+
+    questionChoices.appendChild(choiceNode);
+}
+}
+
+// startBtn.onclick = startQuiz;
+startBtn.addEventListener("click", startQuiz);
